61b80c8369b4d26ac901a838764473f7ff06f217
MaratFoxx <78219329+MaratFoxx@users.noreply.github.com>
2021-03-02 11:25:02 +0400

    feat: changed the structure of the code

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
- let units = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five', 6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine' };
+module.exports = function toReadable (number) {
 
-    let extradezens = { 0 : 'zero', 00 : '', 10 : 'ten', 11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen', 15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen', 19 : 'nineteen', 20 : 'twenty', 30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty', 70 : 'seventy', 80 : 'eighty', 90 : 'ninety' };
-    
-    let dezens = { 0 : '', 2 : 'twenty ', 3 : 'thirty ', 4 : 'forty ', 5 : 'fifty ', 6 : 'sixty ', 7 : 'seventy ', 8 : 'eighty ', 9 : 'ninety ' };
-    
-    let hundreds = { 0 : '', 1 : 'one hundred ', 2 : 'two hundred ', 3 : 'three hundred ', 4 : 'four hundred ', 5 : 'five hundred ', 6 : 'six hundred ', 7 : 'seven hundred ', 8 : 'eight hundred ', 9 : 'nine hundred ' };
-    
+    let array_N_1_19 = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let array_N_20_90 = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
 
-module.exports = function toReadable (num) {
-    if(num === 0) {
-    return 'zero'
-    }
-    let aHundereds = (num - (num % 100))/100;
-let bDezens = num - (aHundereds * 100)
-let aDezens = (bDezens - (bDezens % 10))/10;
-let aUnits = num - aHundereds * 100 - aDezens * 10
-let z = parseInt(String(aDezens) + String(aUnits));
-let b = String(aDezens) + String(aUnits);
-console.log(aHundereds);
-console.log(aDezens);
-console.log(aUnits);
-console.log(z + ' это z');
-console.log(b + ' это b');
-if(b == 00)  {
-console.log('!!!b исключение!!!')
-  return hundreds[aHundereds].slice(0, -1)
-} else if(z == 11 || z == 12 || z == 13 || z == 14 || z == 15 || z == 16 || z == 17 || z == 18 || z == 19 || z == 00 || z == 10 || z % 10 == 0){
-  console.log('!!!z исключение!!!')
-  return hundreds[aHundereds] + extradezens[z]
-} else {
-console.log('z не исключение')
-return hundreds[aHundereds] + dezens[aDezens] + units[aUnits];
-}
-}
+
+  let numeric = number.toString();
+  let humanReadable = '';
+  numeric = numeric.replace(/[\, ]/g,'');
+       if(number === 0 ){
+         humanReadable = 'zero';
+     }
+  else if(number < 20){
+    humanReadable = array_N_1_19[number];
+  }else if(numeric.length < 3){
+    humanReadable =  array_N_20_90[numeric[0]] + ' ' + array_N_1_19[numeric[1]];
+  }else if(numeric.length < 4 ){
+     if( numeric[1] !== '1'){
+      humanReadable = array_N_1_19[numeric[0]] + ' hundred ' + array_N_20_90[numeric[1]] + ' ' + array_N_1_19[numeric[2]];
+     }else{
+      humanReadable =  array_N_1_19[numeric[0]] + ' hundred ' + array_N_1_19[numeric[1] + numeric[2]];
+     } 
+  }
+  humanReadable = humanReadable.replace(/ +/g, ' ').trim();
+  return humanReadable;  
+  };
